4af44ada9ee6eebeabb973bfb013a30003fbf3ab
Федор <arbuzovfp@gmail.com>
2020-07-24 06:05:17 +0300

    fix

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# exallenge-checker README
+# exallenge-checker
 
-This is the README for your extension "exallenge-checker". After writing up a brief description, we recommend including the following sections.
+ngtr nuirtn ngurtng
 
 ## Features
 
@@ -24,42 +24,17 @@ For example:
 
 This extension contributes the following settings:
 
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
+- `myExtension.enable`: enable/disable this extension
+- `myExtension.thing`: set to `blah` to do something
 
 ## Known Issues
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+njkernf nreover egt
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
+nvore inergior
 
 ### 1.0.0
 
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+notin iogtr
--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,56 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "exallenge-checker" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "exallenge-checker" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "exallenge-checker.helloWorld",
+    () => {
+      console.log(vscode.workspace.rootPath);
+      // The code you place here will be executed every time your command is executed
+      var spawn = require("child_process").spawn;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('exallenge-checker.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+      console.log("Spawning child process");
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from exallenge-checker!');
-	});
+      var child = spawn("ls", ["-la"]);
 
-	context.subscriptions.push(disposable);
+      child.on("exit", function () {
+        clearTimeout(to);
+        console.log("Child exited!");
+      });
+
+      child.stdout.on("data", function (data: any) {
+        console.log("stdout: " + data);
+        vscode.window.showInformationMessage(data);
+      });
+
+      child.stderr.on("data", function (data: any) {
+        console.log("stderr: " + data);
+      });
+
+      var to = setTimeout(function () {
+        console.log("Sending sigkill");
+        child.kill();
+      }, 2000);
+      // Display a message box to the user
+      vscode.window.showInformationMessage(
+        "Hello World from exallenge-checker!"
+      );
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
